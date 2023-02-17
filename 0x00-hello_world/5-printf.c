5d513d375b9c254ea06964a351ad0fa630b4c572
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-16 19:00:25 -0800

    my language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A good program that does print lines using print f function
+ * main - A program that can print lines using the print f function
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
