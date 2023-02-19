d4955972a46ddd1b18e97ea5d69e3d5598c6e090
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-18 18:48:58 -0800

    big an low

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - prints both lower and upper case alphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < = 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch < = 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
