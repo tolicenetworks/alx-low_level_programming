87896cd50ece08aacd1ad27b6060e0a53069adc9
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-18 21:27:14 -0800

    sixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This prints the numbers in base of 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char le;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
