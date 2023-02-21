d8b13acc12d69915c97e21af0b4bd921b61690dd
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-20 17:43:51 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * code by tolice
- * Return: 0
+ * main - a program that prints the size of various computer types
+  * Return: 0 (Success)
  */
 int main(void)
 {
 	char a;
-	int a;
-	long int b;
-	long long int c;
+	int b;
+	long int c;
+	long long int d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+}
