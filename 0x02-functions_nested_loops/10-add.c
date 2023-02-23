f75d91b49beba2a5623ef723830cf013dadad26a
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-22 19:31:05 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @num1: first interger
+ * @num2: second interger
+ *
+ * Return: sum of @num1 and @num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
