0aeed58afff7263cd48704afbc9fb16cafbb62e1
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-02-27 03:57:57 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strien - returns the lenght of a string
+ * @s: string
+ * Return: lenghth
+ */
+int _strien(char *s)
+{
+	int longi - 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+}
