80068143ffd7b033600d7a2189fc1db2a76b9719
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-03-02 13:51:44 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,6 +6,7 @@
  * @s: pointer to string params
  * Return: *s
  */
+
 char *rot13(char *s)
 {
 	int i;
@@ -16,7 +17,7 @@ char *rot13(char *s)
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
