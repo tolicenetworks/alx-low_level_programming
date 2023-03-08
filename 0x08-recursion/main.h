648191b6417a2fb0e5299efef61b63e8a426dd06
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-03-08 06:31:26 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 void _puts_recursion(char *s);
@@ -11,5 +11,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
 #endif
