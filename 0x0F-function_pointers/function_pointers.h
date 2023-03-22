ac52a50ee9e0eb0071c687c89b424c94a01fe8ba
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-03-22 16:14:21 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,3 @@
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -11,4 +10,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-
