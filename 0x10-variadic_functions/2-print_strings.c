81f986e8440d044c8e8d5e75eeb86b800ed0653c
Tolice Networks <warikubu_alice@yahoo.co.uk>
2023-03-24 17:13:20 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,6 +30,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	prinf("\n");
+	printf("\n");
 	va_end(strings);
 }
